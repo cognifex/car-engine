817dfafe67b3a714e6c1a79927d25e166182c802
Tim Heimes <info@cognifaktur.ai>
2025-12-09 18:40:31 +0100

    dump-watcher 2025-12-09_18-40-31

--- dump-watcher.sh
+++ dump-watcher.sh
@@ -2,6 +2,9 @@
 
 DUMP_DIR="/opt/car-engine/deploy-dumps"
 
+# Ensure dump directory exists to prevent inotifywait from exiting immediately
+mkdir -p "$DUMP_DIR"
+
 inotifywait -m -e create "$DUMP_DIR" | while read path action file; do
     if [[ "$file" == deploy_dump_*.zip ]]; then
         /opt/car-engine/dump-handler.sh "$file"
